1f328b5cc8952d569ef60a95dd203078258b175c
zlic <MF20320220@smail.nju.edu.cn>
2020-11-24 01:05:00 +0800

    ✨feat(hbase): 完成部分hbase接口

--- src/util/hbaseHelper.js
+++ src/util/hbaseHelper.js
@@ -5,9 +5,42 @@ let client = undefined;
 
 function initClient (callback) {
     client = hbase({ host: hbaseHost, port: hbasePort });
-    client.tables((error) => callback(!error));
+    client.tables((error, tables) => {
+        const success = !error;
+        callback(success);
+        if (success) {
+            console.log('[INFO] tables: ', tables);
+        }
+    });
+}
+
+function getCartoonInfo (id, onfail, onsuccess) {
+    // TODO 等文档确定
+    onfail(id, onsuccess);
+}
+
+function getCartoonRankPath (id, onfail, onsuccess) {
+    // TODO 等文档确定
+    onfail(id, onsuccess);
+}
+
+function getRecommendUsers (id, onfail, onsuccess) {
+    const RELATIVE_USERS_TABLE_NAME = 'relativeUsers';
+    const relativeUsersRow = client.table(RELATIVE_USERS_TABLE_NAME).row(id);
+    const RELATIVE_USERS_COLUMN = 'relative:neighbors';
+    relativeUsersRow.get(RELATIVE_USERS_COLUMN, (error, result) => {
+        if (error || !result) {
+            onfail();
+            return;
+        }
+        const data = result[0];
+        onsuccess(data);
+    });
 }
 
 module.exports = {
-    initClient
+    initClient,
+    getCartoonInfo,
+    getCartoonRankPath,
+    getRecommendUsers
 };
