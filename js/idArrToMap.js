85efbff787d6e4d47b70d117b26dd56186691977
zlic <MF20320220@smail.nju.edu.cn>
2020-11-24 15:22:58 +0800

    create map

---
+++ js/idArrToMap.js
@@ -0,0 +1,14 @@
+function exec (source, target) {
+    const fs = require("fs");
+    const path = require("path");
+    const targetPath = path.resolve(__dirname, target);
+    const arr = require(source);
+    const map = {};
+    arr.forEach(data => {
+        map[data.id] = data;
+    });
+    fs.writeFileSync(targetPath, JSON.stringify(map));
+}
+
+// exec("./../json/cartoonInfoList.json", "./../json/cartoonInfoMap.json");
+exec("./../json/userList.json", "./../json/userMap.json");
